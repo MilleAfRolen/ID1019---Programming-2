e51486b2806ac453a2d39c36e4eb5ef8c1e40182
Mille af Rolén <97411411+MilleAfRolen@users.noreply.github.com>
2022-01-27 00:33:27 +0100

    dogwater

---
+++ Assignments/mips.ex
@@ -0,0 +1,27 @@
+defmodule Emulator do
+
+
+    def run(prgm) do
+        {code, data} = Program.load(prgm)
+        out = Out.new()
+        reg = Registers.new()
+        run(0, code, reg, data, out)
+    end
+
+    def run(pc, code, reg, mem, out) do
+        next = Program.read_instruction(code, pc)
+        case next do
+            :halt -> Out.close(out)
+
+            {:out, rs} ->
+                pc = pc + 4
+                s = Register.read(reg, rs)
+                out = Out.put(out, s)
+                reg = Register.write(reg, rd, s + t) # well, almost
+            run(pc, code, reg, mem, out)
+        
+        :
+    end
+end 
+
+defmodule 
