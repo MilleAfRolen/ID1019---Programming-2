3c25b4c515938261d5dce95b5649f1ab380e4bbe
Mille af Rolén <97411411+MilleAfRolen@users.noreply.github.com>
2022-02-08 01:03:38 +0100

    Update interpreter.ex

--- Assignments/interpreter.ex
+++ Assignments/interpreter.ex
@@ -1,19 +1,29 @@
-defmodule Environment do
+defmodule Env do
 
 
-def new do [] end
+    def new do [] end
 
 
-def add(id, str, env) do
+    def add(id, str, env) do
     [{id, str} | env]
-end
+    end
 
-def lookup(_, []) do nil end
-def lookup(id, env = [h|t]) do
-    {x, _} = h
-    cond do
-        id == x -> h
-        true -> lookup(id, t)
+    def lookup(_, []) do nil end
+    def lookup(id, env = [h|t]) do
+        {x, _} = h
+        cond do
+            id == x -> h
+            true -> lookup(id, t)
+       end
+    end
+
+    def remove(ids, env=[h|t]) do #int klar
+        {x, _} = h
+        cond do
+            ids == x -> t
+            true -> remove(ids, t)
+        end
+
     end
 
 end
